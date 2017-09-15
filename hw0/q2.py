0b46be17fadf52459b979d0a6456456b4b289b2c
Kai-Hsin Wu <r05222003@ntu.edu.tw>
2017-09-15 13:53:20 +0800

    finished q2

--- hw0/q2.py
+++ hw0/q2.py
@@ -1,6 +1,20 @@
-from PIL import *
+from PIL import Image
+from PIL import ImageEnhance
 import os,sys
 
 ipt_file = sys.argv[1]
+#read image
+img = Image.open(ipt_file)
+
+#test:
+#pix1 = list(img.getdata())
+enhancer = ImageEnhance.Brightness(img)
+imgw = enhancer.enhance(0.5)
+#pix2  = list(imgw.getdata())
+imgw.save("Q2.jpg","JPEG")
+
+#for i in range(10):
+#	print(pix1[i],pix2[i])
+
 
 
